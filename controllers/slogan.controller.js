8ff435094168c3d5d30c5889c078b018f7a0d4a1
todorm <todorm@aiidatapro.com>
2021-01-22 15:10:47 +0200

    change name to dat

--- controllers/slogan.controller.js
+++ controllers/slogan.controller.js
@@ -5,16 +5,16 @@ exports.genSlogan = (req, res) => {
   slogan.getSloganList((err, data) => {
   	const random_num = Math.floor(Math.random()*data.length);
   	if (random_num > 5) {
-  		data_f = data.slice(random_num-3, random_num)
+  		data = data.slice(random_num-3, random_num)
   	}
   	else {
-  		data_f = data.slice(random_num, random_num+3)
+  		data = data.slice(random_num, random_num+3)
   	};
     if (err)
       res.status(500).send({
         message:
           err.message || "Some error occurred while retrieving slogans."
       });
-    else res.send(data_f);
+    else res.send(data);
   });
 };
