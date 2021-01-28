79dcaa0655a3c9e181d2642d15ef65adb5b757f3
todorm <todorm@aiidatapro.com>
2021-01-28 17:13:53 +0200

    change token verification

--- middleware/login.js
+++ middleware/login.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 exports.authenticateJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (authHeader) {
-        const token = authHeader.split(' ')[1];
+        const token = authHeader;
         jwt.verify(token, process.env.TokenSecret, (err, user) => {
             if (err) {
                 return res.sendStatus(403);
