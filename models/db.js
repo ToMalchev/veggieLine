87297c707a4995f894b15bd3b81ac03401f51f79
todorm <todorm@aiidatapro.com>
2021-02-16 17:32:34 +0200

    add charset utf-8

--- models/db.js
+++ models/db.js
@@ -6,7 +6,8 @@ const connection = mysql.createConnection({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  database: dbConfig.DB,
+  charset : 'utf8'
 });
 
 // open the MySQL connection
