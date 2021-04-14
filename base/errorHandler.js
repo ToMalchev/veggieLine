a0074aa9037780ec640e2ef9af3dd97b46ded26f
todorm <todorm@aiidatapro.com>
2021-04-14 16:26:21 +0300

    fix error

--- base/errorHandler.js
+++ base/errorHandler.js
@@ -1,16 +1,17 @@
 const baseCR = (err, data, type, operation) => {
 	if (err) {
-		return {status: 500, data: {message: err.message || "Some error occurred while" + operation + type+ "."}
-	} else {
-		return {status: 200, data: data};
+		return {status: 500, data: {message: err.message || "Some error occurred while " + operation + type+ "."}}
 	};
+	return {status: 200, data: data}
+};
 
 const baseRUD = (err, data, type, operation) => {
 	if (err) {
-		return {status: 404, data: {message: err.message || "Not found" + type + "."}} ? err.kind==='not_found' : {status: 500, data: {message: err.message || "Could not" + ${operation} + ${type} + "."}}
-	} else {
-		return {status: 200, data: data};
-	};
+		status = 404 ? err.kind==='not_found' : 500;
+		message = err.message || "Not found " ? err.kind==='not_found' :  "Could not ";
+		return {status: status, data: {message: message + operation + type + "."}}
+	}
+	return {status: 200, data: data}
 };
 
 module.exports = [baseCR, baseRUD]
