52451ff73c9e87db83bf9d0ffaa7304cb8bf9ba4
todorm <todorm@aiidatapro.com>
2021-04-14 16:08:32 +0300

    fix typo

--- base/errorHandler.js
+++ base/errorHandler.js
@@ -7,7 +7,7 @@ const baseCR = (err, data, type, operation) => {
 
 const baseRUD = (err, data, type, operation) => {
 	if (err) {
-		return {status: 404, data: {message: err.message || "Not found" + type + "."} ? err.kind==='not_found' : {status: 500, data: {message: err.message || "Could not" + ${operation} + ${type} + "."}}
+		return {status: 404, data: {message: err.message || "Not found" + type + "."}} ? err.kind==='not_found' : {status: 500, data: {message: err.message || "Could not" + ${operation} + ${type} + "."}}
 	} else {
 		return {status: 200, data: data};
 	};
