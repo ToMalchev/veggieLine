9ed3475103efb287177ed78a71535e99bc6972c5
todorm <todorm@aiidatapro.com>
2021-04-14 16:04:51 +0300

    fix format bug

--- base/errorHandler.js
+++ base/errorHandler.js
@@ -1,13 +1,13 @@
 const baseCR = (err, data, type, operation) => {
 	if (err) {
-		return {status: 500, data: {message: err.message || `Some error occurred while ${operation} ${type}.`}; 
+		return {status: 500, data: {message: err.message || "Some error occurred while" + operation + type+ "."}; 
 	} else {
 		return {status: 200, data: data};
 	};
 
 const baseRUD = (err, data, type, operation) => {
 	if (err) {
-		return {status: 404, data: {message: err.message || `Not found ${type}.`} ? err.kind==='not_found' : {status: 500, data: {message: err.message || `Could not ${operation} ${type}.`}};
+		return {status: 404, data: {message: err.message || "Not found" + type + "."} ? err.kind==='not_found' : {status: 500, data: {message: err.message || "Could not" + ${operation} + ${type} + "."}};
 	} else {
 		return {status: 200, data: data};
 	};
